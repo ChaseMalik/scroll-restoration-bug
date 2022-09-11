db0f0de820dcccb816951452b75797902e85a131
Chase Malik <chase.rs.malik@gmail.com>
2022-09-11 12:51:42 -0700

    Copy repro from codesandbox

---
+++ pages/anotherPage.tsx
@@ -0,0 +1,5 @@
+const AnotherPage = () => {
+  return <div style={{ minHeight: 4000 }}>Now press Back</div>;
+};
+
+export default AnotherPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
+import Link from "next/link";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div style={{ display: "flex", flexDirection: "column", minHeight: 4000 }}>
+      {new Array(1000).fill("Scroll down").map((el, index) => (
+        <div key={index}>{el}</div>
+      ))}
+
+      <Link href="/anotherPage">
+        <a style={{ marginTop: "auto" }}>Go to Another page</a>
+      </Link>
+    </div>
+  );
 }
